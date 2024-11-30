43996e482edc75d372380a796933561465c73a9a
Shaikh Basim <shaikhbasim050@gmail.com>
2024-11-30 19:44:43 +0100

    ss

--- alignment.py
+++ alignment.py
@@ -1,22 +1,10 @@
 import subprocess
 import os
-import tempfile
-from Bio import AlignIO
 import io
 import re
-from tkinter import messagebox
+import tempfile
+from Bio import AlignIO
 from visualization import format_newick_string
-
-def run_alignment(algorithm, fasta_file):
-    if algorithm == "ClustalW":
-        # Run ClustalW alignment
-        return run_clustalw(fasta_file)
-    elif algorithm == "MUSCLE":
-        # Run MUSCLE alignment
-        return run_muscle(fasta_file)
-    else:
-        messagebox.showerror("Error", "Invalid algorithm selected.")
-        return None, None, None
 
 
 def run_clustalw(fasta_file):
@@ -33,15 +21,8 @@ def run_clustalw(fasta_file):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     clustalw2 = os.path.join(script_dir, r"bin\clustalw2")
     command = f"{clustalw2} -INFILE={fasta_file} -OUTFILE={output_file_path}"
-    print(f"Running command: {command}")
+    subprocess.run(command, shell=True, check=True)
     
-    try:
-        subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        messagebox.showerror("Error", f"Error running ClustalW: {str(e)}")
-        return None, None, None
-    
-    # Check if the output file was created and handle it
     if os.path.exists(output_file_path):
         with open(output_file_path, "r") as file:
             alignment_data = io.StringIO(file.read())
@@ -68,8 +49,7 @@ def run_muscle(fasta_file):
 
     try:
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        print("Contents of bin directory:", os.listdir(os.path.join(script_dir, "bin")))
-        muscle_exe = os.path.join(script_dir, "bin", "muscle3.8.31_i86linux64")
+        muscle_exe = os.path.join(script_dir, r"bin\muscle3.8.31_i86win32")
         command = [muscle_exe, '-in', fasta_file, '-out', output_file_path]
         result = subprocess.run(command, capture_output=True, text=True)
         
@@ -77,25 +57,14 @@ def run_muscle(fasta_file):
             with open(output_file_path, "r") as file:
                 alignment_data = io.StringIO(file.read())
             alignment = AlignIO.read(alignment_data, "fasta")
-            guide_tree_file = generate_tree_with_fasttree(output_file_path)
-            if guide_tree_file:
-                with open(guide_tree_file, 'r') as f:
-                    tree_str = f.read()
-                    formatted_tree = format_newick_string(tree_str)
-                    # Write the formatted tree back to file
-                    with open(guide_tree_file, 'w') as f:
-                        f.write(formatted_tree)
-            return alignment, output_file_path, guide_tree_file
+            return alignment, output_file_path
         else:
-            messagebox.showerror("Error", f"Error running MUSCLE: {result.stderr}")
-            return None, None, None
-    except PermissionError:
-        messagebox.showerror("Permission Error", f"Permission denied when trying to write to {output_file_path}")
-        return None, None, None
-    except Exception as e:
-        messagebox.showerror("Error", f"An unexpected error occurred: {str(e)}")
-        return None, None, None
-
+            print("Error running MUSCLE:", result.stderr)
+            return None, None
+            
+    finally:
+        # Files will be cleaned up after the app is done using them
+        pass
 
 def generate_tree_with_fasttree(alignment_file):
     guide_tree_file = re.sub(r"\.aln$", ".dnd", alignment_file)
@@ -111,3 +80,28 @@ def generate_tree_with_fasttree(alignment_file):
     else:
         print("Error generating guide tree:", result.stderr)
         return None
+
+def run_alignment(algorithm, fasta_file):
+    if algorithm == "ClustalW":
+        alignment, output_file, guide_tree_file = run_clustalw(fasta_file)
+        if guide_tree_file:
+            with open(guide_tree_file, 'r') as f:
+                tree_str = f.read()
+                formatted_tree = format_newick_string(tree_str)
+                # Write the formatted tree back to file
+                with open(guide_tree_file, 'w') as f:
+                    f.write(formatted_tree)
+        return alignment, output_file, guide_tree_file
+    elif algorithm == "MUSCLE":
+        alignment, output_file = run_muscle(fasta_file)
+        if alignment:
+            guide_tree_file = generate_tree_with_fasttree(output_file)
+            if guide_tree_file:
+                with open(guide_tree_file, 'r') as f:
+                    tree_str = f.read()
+                    formatted_tree = format_newick_string(tree_str)
+                    # Write the formatted tree back to file
+                    with open(guide_tree_file, 'w') as f:
+                        f.write(formatted_tree)
+            return alignment, output_file, guide_tree_file
+        return alignment, output_file, None
